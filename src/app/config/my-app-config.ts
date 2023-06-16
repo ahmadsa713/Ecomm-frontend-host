e981b0ed5158d86b8e1672461a2e8786adf821cb
AS <ahmadsa713@gmail.com>
2023-06-15 22:02:18 -0400

    pushing

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: 'eu4OaxjMLKUQOgcD4TOwAGAp2FQwlQks',
+        issuer: 'https://dev-nr4ssam5f2zjpm5e/oauth2/default',
+        redirectUri: 'https://127.0.0.1:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
